adbfa90432b22a9e261457eed75878fcd0cd41e8
Jordan <jordan@vitalacy.com>
2025-04-22 19:16:21 -0700

    quicksave

--- main.py
+++ main.py
@@ -50,6 +50,8 @@ last_snapshot = None  # NEW
 def control_servo():
     joystick = connect_controller()
 
+    pygame.event.pump()
+
     # Read RT trigger (usually axis 5 on Xbox controllers)
     rt_value = joystick.get_axis(2)  # Range: -1.0 to 1.0
     throttle = (rt_value + 1) / 2    # Normalize to 0.0 to 1.0
@@ -81,7 +83,6 @@ def esc_sweep():
 
 try:
     while True:
-        pygame.event.pump()
 
         esc_sweep()
         
