ebb8f4a42e56065a3e42092a92859ae6b21bd934
Jordan <jordan@vitalacy.com>
2025-04-27 23:10:00 -0700

    quicksave

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def control_loop():
 
             vehicle_state = new_vehicle_state
 
-            await asyncio.sleep(0.05)
+            await asyncio.sleep(0.1)
     except KeyboardInterrupt:
         print("\n[Shutdown] Stopping ESC and Servos...")
     finally:
