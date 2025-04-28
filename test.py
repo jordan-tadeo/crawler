e4c589d047c7fffa2f1c7cccefccd496a109cf48
Jordan <jordan@vitalacy.com>
2025-04-27 23:37:24 -0700

    quicksave

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-import cv2
+from adafruit_pca9685 import PCA9685
+import board
+import busio
+from adafruit_motor import servo
 
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Failed to open camera")
-else:
-    ret, frame = cap.read()
-    if ret:
-        print("Frame captured successfully")
-    else:
-        print("Failed to capture frame")
-cap.release()
+i2c = busio.I2C(board.SCL, board.SDA)
+pca = PCA9685(i2c)
+pca.frequency = 50
+
+test_servo = servo.Servo(pca.channels[0])
+test_servo.angle = 90  # Move to neutral position
+print("Servo moved to 90 degrees")
