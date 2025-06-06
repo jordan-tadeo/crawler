31aa0ee70c7775b6bf74c2984a97ff54cd568b77
Jordan <jordan@vitalacy.com>
2025-06-06 15:23:25 -0700

    quicksave

--- oakd.py
+++ oakd.py
@@ -40,7 +40,7 @@ class OakD:
         stereo.setExtendedDisparity(False)
         stereo.setRectifyEdgeFillColor(0)
         stereo.setDepthAlign(dai.CameraBoardSocket.RGB)  # Optional
-        stereo.setPostProcessingHardwareResources(numShaves=2, numMemorySlices=3)
+        stereo.setPostProcessingHardwareResources(2, 3)
 
         # stereo.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
 
