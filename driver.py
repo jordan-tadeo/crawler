efaca6b346daddff24b47efd300e145ac5cd9365
Jordan <jordan@vitalacy.com>
2025-06-06 16:31:17 -0700

    quicksave

--- driver.py
+++ driver.py
@@ -18,11 +18,11 @@ class Driver:
 
 
         # Tunable parameters
-        self.forward_speed = 0.05    # very slow forward motion
+        self.forward_speed = 0.02    # very slow forward motion
         self.reverse_speed = -0.5   # very slow backup
         self.turn_speed = 0.7       # light steering during recovery
         self.stuck_threshold = 0.03  # or whatever threshold makes sense for imu
-        self.stuck_timeout = 5.0
+        self.stuck_timeout = 2.5
 
 
     def is_moving(self, imu) -> bool:
