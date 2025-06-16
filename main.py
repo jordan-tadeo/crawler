67bcb3bdfd9ccb902c8f364aea3ac3664e21a801
Jordan <jordan@vitalacy.com>
2025-06-16 10:58:24 -0700

    quicksave

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def control_loop(vc: VehicleController, driver: Driver):
             while True:
                 # Do something
                 await driver.tick()
-                # await vc.throttle_sweep(1100, 1901, 100, 2)
+                await vc.throttle_sweep(1100, 1951, 50, 2)
                 await asyncio.sleep(0.1)
         except KeyboardInterrupt:
             print("\n[Shutdown] Stopping ESC and Servos...")
